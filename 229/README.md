204686a2b6d5920f2516c3d4e47ddaf6d308b0af
Thibault Doutre <doutre@uber.com>
2017-08-08 17:15:35 -0700

    Clean to faster version

---
+++ 229/README.md
@@ -0,0 +1,10 @@
+Iterative solution:
+-------------------
+Provides an intuitive approach to the problem.   
+P(n) is found by performing a deep search over the possible segments to add to the undone polygone.
+The algorithm virtually uses no memory but takes a long time to run (n=60 takes about 24h)
+
+Recursive solution:
+-------------------
+Provides a faster way of computing P(n): n=60 takes about 6 minutes to run.
+Algorithm is described in dynamic.pdf.
--- 229/dynamic.py
+++ 229/dynamic.py
@@ -1,7 +1,6 @@
 import itertools as it
 import numpy as np
 import time
-import itertools
 import progressbar
 
 def minus(a,b):
@@ -16,13 +15,8 @@ def norm(x):
 def norm2(x):
     return int(x[0]*x[0]+x[1]*x[1])
 
-def add_segments(segment1, segment2):
-	return [segment1[0]+segment2[0], segment1[1]+segment2[1]]
-
 def is_between(vector,min,max):
-	bool1 = (min[0]*vector[1]-min[1]*vector[0]) >0
-	bool2 = (max[0]*vector[1]-max[1]*vector[0]) <0
-	return (bool1 and bool2)
+	return (min[0]*vector[1]>min[1]*vector[0]) and (max[0]*vector[1]<max[1]*vector[0])
 
 def possible_segments_fun(n):
     c_max = int(n/2)
@@ -48,71 +42,60 @@ def possible_segments_fun(n):
     angles_inds = np.array(angles).argsort()
     return [res[i] for i in angles_inds]
 
-def path_count(segment, n, min_angle, max_angle, possible_segments, original_segment):
+def path_count(segment, n, min_angle, max_angle, possible_segments):
 	res = 0
-	while len(possible_segments)>0:						
-		next_segment = possible_segments.pop(0)                            				## Get legitimate segment
-		new_segment = add_segments(segment, next_segment)                       		## Get new first segment			
-		new_n = n - norm(segment) - norm(next_segment) + norm(new_segment)								
-		bool1 = (round(new_n*new_n) >= 4*norm2(new_segment))
-		bool2 = is_between(new_segment, original_segment,
-				[-original_segment[0], -original_segment[1]])	
-		bool3 = (new_segment[0]>=0)	
-		if bool1 and bool2 and bool3:			 										## Can polygon be done?
-			if ((norm(new_segment) % 1) == 0):											## Is legitimate finish?			
-				res = res + 1															## Found one new polygon				
-			new_max_angle = [-new_segment[0],-new_segment[1]]																					
-			next_possible_segments = next_possible_segments_fun(						## Get legitimate segments of new segment
-				segment, n, next_segment, new_max_angle, 				
-				possible_segments)							
-			if len(next_possible_segments)>0:											## If there is any legitima, repeat
-				res = res + path_count(				
-					new_segment, new_n, next_segment, 				
-					new_max_angle, next_possible_segments, original_segment)			
-		possible_segments = next_possible_segments_fun(									## Update legitimate segment list
-			segment, n, min_angle, max_angle, possible_segments)
+	while len(possible_segments)>0:		
+		next_segment = possible_segments.pop(0)
+		if is_between(next_segment, min_angle, max_angle):									## Get legitimate segment
+			new_segment = [segment[0]+next_segment[0], segment[1]+next_segment[1]]          ## Get new first segment			
+			new_n = n - norm(segment) - norm(next_segment) + norm(new_segment)
+			if new_segment[0]>=0:	
+				if round(new_n*new_n) >= 4*norm2(new_segment):								## Can polygon be done?
+					if ((norm(new_segment) % 1) == 0):										## Is legitimate finish?			
+						res += 1  															## Found one new polygon				
+					new_max_angle = [-new_segment[0],-new_segment[1]]																				
+					next_possible_segments = next_possible_segments_fun(					## Get legitimate segments of new segment
+						next_segment, new_max_angle, possible_segments)				
+					if next_possible_segments:												## If there is any legitima, repeat
+						res = res + path_count(new_segment, new_n,				
+							next_segment, new_max_angle, next_possible_segments)	
+			possible_segments = next_possible_segments_fun(									## Update legitimate segment list
+				min_angle, max_angle, possible_segments)
 	return res
 
-def next_possible_segments_fun(segment, n, min_angle, max_angle, possible_segments):
+def next_possible_segments_fun(min_angle, max_angle, possible_segments):
 	i=0
 	while i<len(possible_segments):
 		if is_between(possible_segments[i], min_angle, max_angle):
 			break
-		i = i+1
+		i += 1
 	return possible_segments[i:]
 
 def segments_rolls(possible_segments):
     possible_segments_rolls = {}
     for i in range(len(possible_segments)):
         key = possible_segments[i]
-        possible_segments_rolls[tuple(key)] = (np.roll(possible_segments,-i,axis=0)).tolist()
+        if key[0]>0:
+        	possible_segments_rolls[tuple(key)] = (np.roll(possible_segments,-i,axis=0)).tolist()
     return possible_segments_rolls
 
 def P(n):
 	res = 0
 	rolls = segments_rolls(possible_segments_fun(n))
-	bar = progressbar.ProgressBar()
-	for segment_coords in bar(rolls.keys()):
+	keys = rolls.keys()
+	n_iter = len(keys)
+	for i, segment_coords in enumerate(keys):
 		segment = [segment_coords[0], segment_coords[1]]
-		if is_between(segment,[0,-1], [0,1]):
-			possible_segments = rolls[segment_coords]
-			min_angle = segment
-			max_angle = [-segment[0],-segment[1]]
-			next_possible_segments = next_possible_segments_fun(segment, n, min_angle, max_angle, possible_segments)
-			original_segment = segment
-			res = res + path_count(segment, n, min_angle, max_angle, next_possible_segments, original_segment)
+		res = res + path_count(segment, n, segment, [-segment_coords[0], -segment_coords[1]], rolls[segment_coords])
+		print i*1000/n_iter, res
 	return res
 
-def main(n):
-	t0 = time.time()
-	res = P(n)
-	tot_time = time.time()-t0
-	print '\n@@@@@@@@@@@@@@@@@@@@@@@'
-	print '** Result:', res, '**'
-	print '@@@@@@@@@@@@@@@@@@@@@@@'
-	print 'Found in', tot_time, 'seconds.\n'
+print P(30)
 
-main(120)
+
+
+
+
 
 
 
